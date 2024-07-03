76f96102c680f479d98c59a7e6a7510b7075f657
AI Discovery Academy <152899858+ai-camps@users.noreply.github.com>
2024-07-03 17:31:22 -0400

    Update main.cpp

--- Project_7/main.cpp
+++ Project_7/main.cpp
@@ -1,3 +1,7 @@
+// **********************************
+// * Project 7
+// * Connect a OLED (SS1306) and display WiFi SSID, IP address, date, time and LED status
+// **********************************
 // * Libraries
 #include <Arduino.h>   // Includes the core Arduino functions for the ESP32
 #include <WiFi.h>      // Includes the WiFi library to connect to WiFi networks
@@ -25,10 +29,9 @@ constexpr unsigned long ALARM_TONE_DURATION_MS = 100;   // Duration of each alar
 constexpr unsigned long ALARM_TOTAL_DURATION_MS = 3000; // Total duration of the alarm
 
 const int greenLedChannel = 0;  // PWM channel for Green LED
-const int redLedChannel = 1;   // PWM channel for Red LED
-const int buzzerChannel = 2;   // PWM channel for Buzzer
-const int pwmFrequency = 5000; // Frequency for PWM
-const int pwmResolution = 8;   // 8-bit resolution (0-255)
+const int buzzerChannel = 2;    // PWM channel for Buzzer
+const int pwmFrequency = 5000;  // Frequency for PWM
+const int pwmResolution = 8;    // 8-bit resolution (0-255)
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -67,7 +70,7 @@ void setup()
     display.print(RESTART_DELAY_MS / 1000);
     display.println(" seconds.");
     display.display();
-    ledcWrite(redLedChannel, 255); // Turn on Red LED
+    ledcWrite(greenLedChannel, 0); // Turn off Green LED
     delay(RESTART_DELAY_MS);       // Wait before restarting
     ESP.restart();                 // Reboot the MCU
   }
@@ -94,7 +97,7 @@ void setup()
     display.print(RESTART_DELAY_MS / 1000);
     display.println(" seconds.");
     display.display();
-    ledcWrite(redLedChannel, 255); // Turn on Red LED
+    ledcWrite(greenLedChannel, 0); // Turn off Green LED
     delay(RESTART_DELAY_MS);       // Wait before restarting
     ESP.restart();                 // Reboot the MCU
   }
@@ -110,7 +113,7 @@ void setup()
     display.print(RESTART_DELAY_MS / 1000);
     display.println(" seconds.");
     display.display();
-    ledcWrite(redLedChannel, 255); // Turn on Red LED
+    ledcWrite(greenLedChannel, 0); // Turn off Green LED
     delay(RESTART_DELAY_MS);       // Wait before restarting
     ESP.restart();                 // Reboot the MCU
   }
@@ -126,7 +129,6 @@ void loop()
   if (WiFi.status() == WL_CONNECTED)
   {
     ledcWrite(greenLedChannel, 255); // Set Green LED brightness to maximum
-    ledcWrite(redLedChannel, 0);     // Turn off Red LED
     Serial.println("WiFi is connected.");
     display.clearDisplay();
     display.setCursor(0, 0);
@@ -142,7 +144,6 @@ void loop()
     display.println("Attempting to reconnect...");
     display.display();
     ledcWrite(greenLedChannel, 0); // Turn off Green LED
-    ledcWrite(redLedChannel, 255); // Turn on Red LED
     int retryCount = 0;            // Initialize retry count
     bool reconnected = false;      // Initialize reconnection status
 
@@ -165,7 +166,6 @@ void loop()
       {
         reconnected = true;              // Set reconnection status to true
         ledcWrite(greenLedChannel, 255); // Set Green LED brightness to maximum
-        ledcWrite(redLedChannel, 0);     // Turn off Red LED
         Serial.println("Reconnected to WiFi successfully!");
         display.clearDisplay();
         display.setCursor(0, 0);
@@ -189,7 +189,6 @@ void loop()
       display.print(RESTART_DELAY_MS / 1000);
       display.println(" seconds.");
       display.display();
-      ledcWrite(redLedChannel, 255); // Turn on Red LED
       delay(RESTART_DELAY_MS);       // Wait before restarting
       ESP.restart();                 // Reboot the MCU
     }
@@ -221,8 +220,6 @@ void loop()
   // Print LED status
   display.print("Green LED: ");
   display.println(ledcRead(greenLedChannel) ? "On" : "Off");
-  display.print("Red LED: ");
-  display.println(ledcRead(redLedChannel) ? "On" : "Off");
 
   display.display();
 
@@ -388,9 +385,6 @@ void initPWM()
   ledcSetup(greenLedChannel, pwmFrequency, pwmResolution);
   ledcAttachPin(GREEN_LED_PIN, greenLedChannel);
 
-  ledcSetup(redLedChannel, pwmFrequency, pwmResolution);
-  ledcAttachPin(RED_LED_PIN, redLedChannel);
-
   // Configure Buzzer PWM functionalities
   ledcSetup(buzzerChannel, pwmFrequency, pwmResolution);
   ledcAttachPin(BUZZER_PIN, buzzerChannel);
@@ -402,14 +396,12 @@ void buzzerAndBlinkAlarm()
 
     while (millis() - startTime < ALARM_TOTAL_DURATION_MS)
     {
-        // Play high frequency and turn on Red LED
+        // Play high frequency
         ledcWriteTone(buzzerChannel, ALARM_HIGH_FREQUENCY);
-        ledcWrite(redLedChannel, 255); // Turn on Red LED
         delay(ALARM_TONE_DURATION_MS);
 
-        // Play low frequency and turn off Red LED
+        // Play low frequency
         ledcWriteTone(buzzerChannel, ALARM_LOW_FREQUENCY);
-        ledcWrite(redLedChannel, 0); // Turn off Red LED
         delay(ALARM_TONE_DURATION_MS);
     }
 
